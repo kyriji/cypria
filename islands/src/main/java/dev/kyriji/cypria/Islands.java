5cb2c8a3e5384f11d07b47cd8dcae4bd8a8ec3c9
wiji <wijigaming.yt@gmail.com>
2025-03-22 16:34:12 -0700

    initial commit

---
+++ islands/src/main/java/dev/kyriji/cypria/Islands.java
@@ -0,0 +1,17 @@
+package dev.kyriji.cypria;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Islands extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

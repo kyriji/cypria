ec7d24e6144d4c7754a688307d3334b601dd4297
Timothy <timothyrwashburn@gmail.com>
2025-03-24 17:49:27 -0700

    fix reflection init system

--- common-minecraft/src/main/java/dev/kyriji/commonmc/cypria/misc/ReflectionUtils.java
+++ common-minecraft/src/main/java/dev/kyriji/commonmc/cypria/misc/ReflectionUtils.java
@@ -2,68 +2,66 @@ package dev.kyriji.commonmc.cypria.misc;
 
 import java.io.File;
 import java.lang.reflect.Modifier;
-import java.net.URL;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 public class ReflectionUtils {
 	public static <T> List<T> initPackage(String packageName, Class<T> type) {
-		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-		String path = packageName.replace('.', '/');
+		System.out.println("attempting to initialize package: " + packageName);
+		List<T> instances = new ArrayList<>();
 
-		URL resource = classLoader.getResource(path);
-		if (resource == null) {
-			System.out.println("package not found: " + packageName);
-			return new ArrayList<>();
+		try {
+			File pluginsDir = new File("plugins");
+			if (pluginsDir.exists() && pluginsDir.isDirectory()) {
+				for (File file : pluginsDir.listFiles((dir, name) -> name.endsWith(".jar"))) {
+					try (JarFile jarFile = new JarFile(file)) {
+						Enumeration<JarEntry> entries = jarFile.entries();
+						String path = packageName.replace('.', '/');
+
+						while (entries.hasMoreElements()) {
+							JarEntry entry = entries.nextElement();
+							String entryName = entry.getName();
+
+							if (entryName.startsWith(path + "/") && entryName.endsWith(".class")) {
+								String className = entryName.substring(0, entryName.length() - 6).replace('/', '.');
+								instances.addAll(instantiateClass(className, type));
+							}
+						}
+					} catch (Exception e) {
+						System.out.println("error scanning jar " + file.getName() + ": " + e.getMessage());
+					}
+				}
+			}
+		} catch (Exception e) {
+			System.out.println("error during classpath scan: " + e);
 		}
 
-		File directory = new File(resource.getFile());
-		List<T> instances = recursivelyInstantiate(directory, packageName, type);
-
-		System.out.println("found " + instances.size() + " classes in package " + packageName);
+		System.out.println("instantiated " + instances.size() + " class" + (instances.size() == 1 ? "" : "es") +
+				" in package " + packageName);
 		return instances;
 	}
 
-	private static <T> List<T> recursivelyInstantiate(File directory, String packageName, Class<T> type) {
+	private static <T> List<T> instantiateClass(String className, Class<T> type) {
 		List<T> instances = new ArrayList<>();
 
-		if (!directory.exists()) {
-			System.out.println("directory does not exist: " + directory.getAbsolutePath());
-			return instances;
-		}
+		try {
+			Class<?> clazz = Class.forName(className);
 
-		File[] files = directory.listFiles();
-		if (files == null) {
-			System.out.println("unable to list files in directory: " + directory.getAbsolutePath());
-			return instances;
-		}
-
-		for (File file : files) {
-			if (file.isDirectory()) {
-				instances.addAll(recursivelyInstantiate(file,
-						packageName + "." + file.getName(), type));
-			} else if (file.getName().endsWith(".class")) {
-				String className = packageName + "." +
-						file.getName().substring(0, file.getName().length() - 6);
-
-				try {
-					Class<?> clazz = Class.forName(className);
-
-					if (clazz.isInterface() || Modifier.isAbstract(clazz.getModifiers()) ||
-							!type.isAssignableFrom(clazz)) {
-						System.out.println("skipping " + className + " (not compatible with " + type.getName() + ")");
-						continue;
-					}
-
-					Object instance = clazz.getDeclaredConstructor().newInstance();
-					instances.add(type.cast(instance));
-					System.out.println("instantiated " + className);
-				} catch (NoSuchMethodException e) {
-					System.out.println("no no-args constructor found for " + className);
-				} catch (Exception e) {
-					System.out.println("error instantiating " + className + ": " + e.getMessage());
-				}
+			if (clazz.isInterface() || Modifier.isAbstract(clazz.getModifiers()) || !type.isAssignableFrom(clazz)) {
+				System.out.println("skipping " + className + " (not compatible with " + type.getName() + ")");
+				return instances;
 			}
+
+			Object instance = clazz.getDeclaredConstructor().newInstance();
+			instances.add(type.cast(instance));
+			System.out.println("instantiated: " + className);
+		} catch (NoSuchMethodException e) {
+			System.out.println("no empty constructor found for " + className);
+		} catch (Exception e) {
+			System.out.println("error instantiating " + className + ": " + e.getMessage());
 		}
 
 		return instances;
